9a296c31acf5eef646233d6b1d79b9da2d0a90eb
Greg Woodcock <greg@yoomee.com>
2014-08-18 16:14:03 +0100

    Fix funny characters on success page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
-  get 'signup_success' => 'home#signup_success'
+  get 'signup-success' => 'home#signup_success'
 end
